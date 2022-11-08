a3d9751966cbe95ac887a6700d4466fe6d68c983
Salim <salim.hassan.welleat@gmail.com>
2022-11-08 21:39:11 +0100

    Using Ionic Element like Normal Html

---
+++ app.js
@@ -0,0 +1,8 @@
+const reasonInput = document.querySelector("input-reason");
+const amountInput = document.querySelector("input-amount");
+const cancelBtn = document.querySelector("#btn-cancel");
+const confirmlBtn = document.querySelector("#btn-confirm");
+
+confirmlBtn.addEventListener("click", () => {
+  console.log("It works");
+});
