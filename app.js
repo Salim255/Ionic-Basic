fa7d311f4ac405b244e770217bb7a8008a6e8889
Salim <salim.hassan.welleat@gmail.com>
2022-11-08 21:50:14 +0100

    Validating User Input

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const reasonInput = document.querySelector("input-reason");
-const amountInput = document.querySelector("input-amount");
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
 const cancelBtn = document.querySelector("#btn-cancel");
 const confirmlBtn = document.querySelector("#btn-confirm");
 
 confirmlBtn.addEventListener("click", () => {
-  console.log("It works");
+  const enteredReason = reasonInput.value;
+  const enteredAmount = amountInput.value;
+
+  if (
+    enteredReason.trim().length <= 0 ||
+    enteredAmount <= 0 ||
+    enteredAmount.trim().length <= 0
+  ) {
+    return;
+  }
+
+  console.log(enteredAmount, enteredReason);
 });
