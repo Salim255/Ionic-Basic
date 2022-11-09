4efe72b90a0614c4b3583fa23dc5bfd82a2f03d0
Salim <salim.hassan.welleat@gmail.com>
2022-11-09 11:23:11 +0100

    Add : Ionic In Angular App

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Ionic-Basic
+
+## ionic start => to create new project
+
+## ionic serve
+
+## ng generate component or ng g c or ionic generate page
---
+++ ionic-angular/src/app/recipes/recipes.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.page.html',
+  styleUrls: ['./recipes.page.scss'],
+})
+export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl:
+        'https://images.unsplash.com/photo-1599921841143-819065a55cc6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2831&q=80',
+      ingredients: ['French Frites', 'Pork Meat', 'Salad'],
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl:
+        'https://images.unsplash.com/photo-1589227365533-cee630bd59bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      ingredients: ['Spaghetti', 'Meat', 'Tomat'],
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
